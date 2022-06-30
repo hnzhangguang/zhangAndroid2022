9f1eb20f5ec2b349b6188a91df8026503f4e542d
zhangguang <zhangguang@yonyou.com>
2022-06-30 08:55:35 +0800

    新增 component 和 plugin module

--- appBase/src/main/java/com/zhang/zhangandroid/basecomponent/ImageViewActivity.java
+++ appBase/src/main/java/com/zhang/zhangandroid/basecomponent/ImageViewActivity.java
@@ -6,6 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.zhang.zhangandroid.R;
 
+
+
+/**
+ * 简介:
+ * 常用属性:
+ *
+ *
+ */
 public class ImageViewActivity extends AppCompatActivity {
 
     @Override
