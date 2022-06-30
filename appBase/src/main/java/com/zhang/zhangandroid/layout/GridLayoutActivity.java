210d13dbdb528ba82fa2640dfed25064751f915f
zhangguang <zhangguang@yonyou.com>
2022-06-30 09:21:10 +0800

    gradle 从7.0 降到 4.2 报 Cannot resolve external dependency androidx.appcompat:appcompat:1.2.0 becaus 问题解决方案

--- appBase/src/main/java/com/zhang/zhangandroid/layout/GridLayoutActivity.java
+++ appBase/src/main/java/com/zhang/zhangandroid/layout/GridLayoutActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.zhang.zhangandroid.R;
+
 public class GridLayoutActivity extends AppCompatActivity {
 
     @Override
