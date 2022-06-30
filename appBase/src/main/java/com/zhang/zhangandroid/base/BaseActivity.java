c76477ba68bfba9fdff80aec03efc550a060ba45
zhangguang <zhangguang@yonyou.com>
2022-06-30 20:06:05 +0800

    ViewHolder

--- appBase/src/main/java/com/zhang/zhangandroid/base/BaseActivity.java
+++ appBase/src/main/java/com/zhang/zhangandroid/base/BaseActivity.java
@@ -9,29 +9,29 @@ import androidx.appcompat.app.AppCompatActivity;
 /**
  * 简介: activity基类
  * 功能: 1, 公共方法
- *       2,工具方法等
- *  作者: zhangg
+ * 2,工具方法等
+ * 作者: zhangg
  */
-public class BaseActivity  extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity {
 
 
     /**
      * 简介: 打开activity
-     *  作者: zhangg
+     * 作者: zhangg
      */
-    public void openActivity(String jumpString){
-        ComponentName cn = new ComponentName(getApplication().getPackageName(),jumpString) ;
-        Intent intent = new Intent() ;
-        intent.setComponent(cn) ;
-        startActivity(intent) ;
+    public void openActivity(String jumpString) {
+        ComponentName cn = new ComponentName(getApplication().getPackageName(), jumpString);
+        Intent intent = new Intent();
+        intent.setComponent(cn);
+        startActivity(intent);
     }
 
 
-    public void logZhang(Object log){
-        if (null == log){
-            Log.e("mmmm","参数 log is null");
-        }else {
-            Log.e("mmmm",log.toString());
+    public void logZhang(Object log) {
+        if (null == log) {
+            Log.e("mmmm", "参数 log is null");
+        } else {
+            Log.e("mmmm", log.toString());
         }
     }
 
--- appBase/src/main/java/com/zhang/zhangandroid/base/MyListViewSimpleAdapter.java
+++ appBase/src/main/java/com/zhang/zhangandroid/base/MyListViewSimpleAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.HeaderViewListAdapter;
 import android.widget.TextView;
 
 import com.zhang.zhangandroid.R;
@@ -12,41 +13,59 @@ import com.zhang.zhangandroid.R;
 import java.util.List;
 
 /**
-     * 简介: listView适配器
-     *  作者: zhangg
-     */
+ * 简介: listView适配器
+ * 作者: zhangg
+ */
 public class MyListViewSimpleAdapter extends BaseAdapter {
 
-        List<String> datas ; // 数据集
-        Context context; // 上下文
+    List<String> datas; // 数据集
+    Context context; // 上下文
 
-        public MyListViewSimpleAdapter(List<String> datas, Context context){
-            this.datas = datas;
-            this.context = context;
-        }
+    public MyListViewSimpleAdapter(List<String> datas, Context context) {
+        this.datas = datas;
+        this.context = context;
+    }
 
-        @Override
-        public int getCount() {
-            return null == datas ? 0: datas.size();
-        }
+    @Override
+    public int getCount() {
+        return null == datas ? 0 : datas.size();
+    }
 
-        @Override
-        public Object getItem(int i) {
-            return datas.get(i);
-        }
+    @Override
+    public Object getItem(int i) {
+        return datas.get(i);
+    }
 
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
 
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+
+        ViewHolder holder = null;
+        if (convertView == null) {
+            holder = new ViewHolder();
             LayoutInflater inflater =
                     (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View inflate =inflater.inflate(R.layout.mainactivity_list_item, null);
-            TextView textView = inflate.findViewById(R.id.activity_list_item);
-            textView.setText(getItem(i).toString());
-            return inflate;
+            convertView = inflater.inflate(R.layout.mainactivity_list_item, null);
+            holder.tv = convertView.findViewById(R.id.activity_list_item);;
+            convertView.setTag(holder);
+        }else {
+            holder = (ViewHolder) convertView.getTag();
         }
+
+        holder.tv.setText(getItem(i).toString());
+        return convertView;
     }
+
+
+    public final class ViewHolder {
+        public TextView tv;//联系人名
+    }
+
+
+}
+
+
