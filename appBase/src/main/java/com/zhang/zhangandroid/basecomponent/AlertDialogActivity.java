e5c2022ae4e8baf9b57a23d5af0e288c606b0c57
zhangguang <zhangguang@yonyou.com>
2022-07-03 19:27:44 +0800

    常用 dialog 总结

--- appBase/src/main/java/com/zhang/zhangandroid/basecomponent/AlertDialogActivity.java
+++ appBase/src/main/java/com/zhang/zhangandroid/basecomponent/AlertDialogActivity.java
@@ -1,10 +1,19 @@
 package com.zhang.zhangandroid.basecomponent;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.zhang.zhangandroid.R;
+
+import java.util.ArrayList;
 
 public class AlertDialogActivity extends AppCompatActivity {
 
@@ -13,4 +22,262 @@ public class AlertDialogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert_dialog);
     }
+
+
+    //普通dialog
+    private void showNormalDialog(){
+        /* @setIcon 设置对话框图标
+         * @setTitle 设置对话框标题
+         * @setMessage 设置对话框消息提示
+         * setXXX方法返回Dialog对象，因此可以链式设置属性
+         */
+        final AlertDialog.Builder normalDialog =
+                new AlertDialog.Builder(AlertDialogActivity.this);
+        normalDialog.setIcon(R.drawable.ic_launcher_background);
+        normalDialog.setTitle("我是一个普通Dialog");
+        normalDialog.setMessage("你要点击哪一个按钮呢?");
+        normalDialog.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //...To-do
+                    }
+                });
+        normalDialog.setNegativeButton("关闭",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //...To-do
+                    }
+                });
+        // 显示
+        normalDialog.show();
+    }
+
+
+
+    /* @setNeutralButton 设置中间的按钮
+     * 若只需一个按钮，仅设置 setPositiveButton 即可
+     */
+    private void showMultiBtnDialog(){
+        AlertDialog.Builder normalDialog =
+                new AlertDialog.Builder(AlertDialogActivity.this);
+        normalDialog.setIcon(R.drawable.ic_launcher_background);
+        normalDialog.setTitle("我是一个普通Dialog").setMessage("你要点击哪一个按钮呢?");
+        normalDialog.setPositiveButton("按钮1",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // ...To-do
+                    }
+                });
+        normalDialog.setNeutralButton("按钮2",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // ...To-do
+                    }
+                });
+        normalDialog.setNegativeButton("按钮3", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // ...To-do
+            }
+        });
+        // 创建实例并显示
+        normalDialog.show();
+    }
+
+
+    // 列表dialog
+    private void showListDialog() {
+        final String[] items = { "我是1","我是2","我是3","我是4" };
+        AlertDialog.Builder listDialog =
+                new AlertDialog.Builder(AlertDialogActivity.this);
+        listDialog.setTitle("我是一个列表Dialog");
+        listDialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // which 下标从0开始
+                // ...To-do
+                Toast.makeText(AlertDialogActivity.this,
+                        "你点击了" + items[which],
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        listDialog.show();
+    }
+
+
+    int yourChoice;
+    // 单选dialog
+    private void showSingleChoiceDialog(){
+        final String[] items = { "我是1","我是2","我是3","我是4" };
+        yourChoice = -1;
+        AlertDialog.Builder singleChoiceDialog =
+                new AlertDialog.Builder(AlertDialogActivity.this);
+        singleChoiceDialog.setTitle("我是一个单选Dialog");
+        // 第二个参数是默认选项，此处设置为0
+        singleChoiceDialog.setSingleChoiceItems(items, 0,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        yourChoice = which;
+                    }
+                });
+        singleChoiceDialog.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (yourChoice != -1) {
+                            Toast.makeText(AlertDialogActivity.this,
+                                    "你选择了" + items[yourChoice],
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        singleChoiceDialog.show();
+    }
+
+
+    ArrayList<Integer> yourChoices = new ArrayList<>();
+
+    // 多选dialog
+    private void showMultiChoiceDialog() {
+        final String[] items = { "我是1","我是2","我是3","我是4" };
+        // 设置默认选中的选项，全为false默认均未选中
+        final boolean initChoiceSets[]={false,false,false,false};
+        yourChoices.clear();
+        AlertDialog.Builder multiChoiceDialog =
+                new AlertDialog.Builder(AlertDialogActivity.this);
+        multiChoiceDialog.setTitle("我是一个多选Dialog");
+        multiChoiceDialog.setMultiChoiceItems(items, initChoiceSets,
+                new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which,
+                                        boolean isChecked) {
+                        if (isChecked) {
+                            yourChoices.add(which);
+                        } else {
+                            yourChoices.remove(which);
+                        }
+                    }
+                });
+        multiChoiceDialog.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int size = yourChoices.size();
+                        String str = "";
+                        for (int i = 0; i < size; i++) {
+                            str += items[yourChoices.get(i)] + " ";
+                        }
+                        Toast.makeText(AlertDialogActivity.this,
+                                "你选中了" + str,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        multiChoiceDialog.show();
+    }
+
+
+
+
+
+    // 等待dialog
+    private void showWaitingDialog() {
+        /* 等待Dialog具有屏蔽其他控件的交互能力
+         * @setCancelable 为使屏幕不可点击，设置为不可取消(false)
+         * 下载等事件完成后，主动调用函数关闭该Dialog
+         */
+        ProgressDialog waitingDialog=
+                new ProgressDialog(AlertDialogActivity.this);
+        waitingDialog.setTitle("我是一个等待Dialog");
+        waitingDialog.setMessage("等待中...");
+        waitingDialog.setIndeterminate(true);
+        waitingDialog.setCancelable(false);
+        waitingDialog.show();
+    }
+
+
+
+    /**
+     * 简介: 自定义dialog
+     *  作者:
+     */
+    private void showCustomizeDialog() {
+        /* @setView 装入自定义View ==> R.layout.dialog_customize
+         * 由于dialog_customize.xml只放置了一个EditView，因此和图8一样
+         * dialog_customize.xml可自定义更复杂的View
+         */
+        AlertDialog.Builder customizeDialog =
+                new AlertDialog.Builder(AlertDialogActivity.this);
+        final View dialogView = LayoutInflater.from(AlertDialogActivity.this)
+                .inflate(R.layout.dialog_customize,null);
+        customizeDialog.setTitle("我是一个自定义Dialog");
+        customizeDialog.setView(dialogView);
+        customizeDialog.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 获取EditView中的输入内容
+                        EditText edit_text =
+                                (EditText) dialogView.findViewById(R.id.edit_text);
+                        Toast.makeText(AlertDialogActivity.this,
+                                edit_text.getText().toString(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        customizeDialog.show();
+    }
+
+
+    /* 复写Builder的create和show函数，可以在Dialog显示前实现必要设置
+     * 例如初始化列表、默认选项等
+     * @create 第一次创建时调用
+     * @show 每次显示时调用
+     */
+    private void showListDialog2() {
+        final String[] items = { "我是1","我是2","我是3","我是4" };
+        AlertDialog.Builder listDialog =
+                new AlertDialog.Builder(AlertDialogActivity.this){
+
+                    @Override
+                    public AlertDialog create() {
+                        items[0] = "我是No.1";
+                        return super.create();
+                    }
+
+                    @Override
+                    public AlertDialog show() {
+                        items[1] = "我是No.2";
+                        return super.show();
+                    }
+                };
+        listDialog.setTitle("我是一个列表Dialog");
+        listDialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // ...To-do
+            }
+        });
+        /* @setOnDismissListener Dialog销毁时调用
+         * @setOnCancelListener Dialog关闭时调用
+         */
+        listDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            public void onDismiss(DialogInterface dialog) {
+                Toast.makeText(getApplicationContext(),
+                        "Dialog被销毁了",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        listDialog.show();
+    }
+
+
+
+
+
+
+
 }
