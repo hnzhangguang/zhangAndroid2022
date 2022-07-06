2882e7e7ca2f20b5779b328f68fdd700242cbd33
zhangguang <zhangguang@yonyou.com>
2022-07-06 18:58:18 +0800

    JsBridgeActivity

---
+++ appBase/src/main/java/com/zhang/zhangandroid/js/JsBridgeActivity.java
@@ -0,0 +1,16 @@
+package com.zhang.zhangandroid.js;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zhang.zhangandroid.R;
+
+public class JsBridgeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_js_brige);
+    }
+}
--- appBase/src/main/java/com/zhang/zhangandroid/util/FileHelper.java
+++ appBase/src/main/java/com/zhang/zhangandroid/util/FileHelper.java
@@ -2,9 +2,11 @@ package com.zhang.zhangandroid.util;
 
 import android.content.Context;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class FileHelper {
 
@@ -48,6 +50,20 @@ public class FileHelper {
     }
 
 
+
+
+    //从流中读取数据
+    public static byte[] read(InputStream inStream) throws Exception{
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while((len = inStream.read(buffer)) != -1)
+        {
+            outStream.write(buffer,0,len);
+        }
+        inStream.close();
+        return outStream.toByteArray();
+    }
 
 
 }
