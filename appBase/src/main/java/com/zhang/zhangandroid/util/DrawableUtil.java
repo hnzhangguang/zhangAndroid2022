1a79ed516e398ec92c5c48a0d45d03127c8c4ff0
zhangguang <zhangguang@yonyou.com>
2022-07-17 18:01:09 +0800

    截屏

--- appBase/src/main/java/com/zhang/zhangandroid/util/DrawableUtil.java
+++ appBase/src/main/java/com/zhang/zhangandroid/util/DrawableUtil.java
@@ -10,16 +10,21 @@ import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -238,9 +243,70 @@ public class DrawableUtil {
 
 
     // 抠图使用方法
-    public void corpBitmap(Context context,Bitmap bitmap){
+    public void corpBitmap(Context context, Bitmap bitmap) {
         Bitmap bitmap1 = bitmap;
-        Bitmap bitmap2 = Bitmap.createBitmap(bitmap1,100,100,200,200);
+        Bitmap bitmap2 = Bitmap.createBitmap(bitmap1, 100, 100, 200, 200);
+    }
+
+    //截屏
+    public void jiePing(Activity context) {
+
+        //截屏
+        Runnable action = new Runnable() {
+
+            Bitmap bitmap = null;
+            ByteArrayOutputStream byteOut = null;
+            @Override
+            public void run() {
+                final View contentView = context.getWindow().getDecorView();
+                try {
+                    Log.e("mmmm", contentView.getHeight() + ":" + contentView.getWidth());
+                    bitmap = Bitmap.createBitmap(contentView.getWidth(),
+                            contentView.getHeight(), Bitmap.Config.ARGB_4444);
+                    contentView.draw(new Canvas(bitmap));
+                    byteOut = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteOut);
+                    savePic(context,bitmap, "sdcard/short.png");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (null != byteOut)
+                            byteOut.close();
+                        if (null != bitmap && !bitmap.isRecycled()) {
+//                            bitmap.recycle();
+                            bitmap = null;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        };
+        try {
+            action.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void savePic(Activity context,Bitmap b, String strFileName) {
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(strFileName);
+            if (null != fos) {
+                boolean success = b.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                fos.flush();
+                fos.close();
+                if (success)
+                    Toast.makeText(context, "截屏成功", Toast.LENGTH_SHORT).show();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
